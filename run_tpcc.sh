1e2475e7222e4ab51d8ca44f29969cda680e8b89
Cesar Avalos <cesar.avalos3@gmail.com>
2022-05-04 18:04:28 -0400

    Update run_tpcc to support no thread arguments

--- run_tpcc.sh
+++ run_tpcc.sh
@@ -1,1 +1,8 @@
-./tpcc.lua --pgsql-user=sbtest --pgsql-db=sbtest --time=5 --events=250 --threads=$1 --report-interval=1 --tables=1 --scale=1 --use_fk=0 --trx_level=RC --db-driver=pgsql --pgsql-password=password run
+threads=1
+if [ -z "$1" ]; then
+    # "No argument supplied; Assuming 1 thread"
+    threads=1
+else
+    threads=$1
+fi
+./tpcc.lua --pgsql-user=sbtest --pgsql-db=sbtest --time=5 --events=250 --threads=$threads --report-interval=1 --tables=1 --scale=1 --use_fk=0 --trx_level=RC --db-driver=pgsql --pgsql-password=password run
